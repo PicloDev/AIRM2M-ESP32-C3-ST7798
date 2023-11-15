b74c126c21da769cea20d7a5a7b59b0080519c14
Piccolo-Work <1352324265@qq.com>
2023-11-15 10:03:25 +0800

    lvgl初步驱动成功

--- src/main.cpp
+++ src/main.cpp
@@ -1,112 +1,149 @@
-#include <Arduino.h>
+/* ----------------- harware ---------------- */
+/*
+BLK 11
+CS 07
+DC 06
+RES 10
+DSDA 03
+SCL 02
+*/
+/*Using LVGL with Arduino requires some extra steps:
+ *Be sure to read the docs here: https://docs.lvgl.io/master/get-started/platforms/arduino.html  */
+
 #include <lvgl.h>
-#include <iostream>
-#include "TFT_eSPI.h"
-#include "gui_guider.h"
+#include <TFT_eSPI.h>
 
-lv_ui guider_ui;
-using namespace std;
-string location, temperature, weather;
+/*To use the built-in examples and demos of LVGL uncomment the includes below respectively.
+ *You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
+ Note that the `lv_examples` library is for LVGL v7 and you shouldn't install it for this version (since LVGL v8)
+ as the examples and demos are now part of the main LVGL library. */
 
+/*Change to your screen resolution*/
 static const uint16_t screenWidth = 240;
 static const uint16_t screenHeight = 240;
 
-static lv_disp_draw_buf_t draw_buf;                  // 初始化显示缓冲区
-static lv_color_t buf_1[screenWidth * screenHeight]; // 用于LVGL使用的缓冲区
-
-lv_obj_t *screenMain; // 指向所用屏幕的实例化对象
-lv_obj_t *labelName;
-lv_obj_t *labelTime;
-lv_obj_t *labelLocation;
-lv_obj_t *labelWeather;
-lv_obj_t *labelTemperature;
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[screenWidth * screenHeight / 10];
 
 TFT_eSPI tft = TFT_eSPI(screenWidth, screenHeight); /* TFT instance */
 
-void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+#if LV_USE_LOG != 0
+/* Serial debugging */
+void my_print(const char *buf)
+{
+    Serial.printf(buf);
+    Serial.flush();
+}
+#endif
+
+/* Display flushing */
+void my_disp_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p)
 {
     uint32_t w = (area->x2 - area->x1 + 1);
     uint32_t h = (area->y2 - area->y1 + 1);
 
     tft.startWrite();
     tft.setAddrWindow(area->x1, area->y1, w, h);
-    tft.pushColors(&color_p->full, w * h, true);
+    tft.pushColors((uint16_t *)&color_p->full, w * h, true);
     tft.endWrite();
 
-    lv_disp_flush_ready(disp);
+    lv_disp_flush_ready(disp_drv);
 }
+
+// /*Read the touchpad*/
+// void my_touchpad_read(lv_indev_drv_t *indev_drv, lv_indev_data_t *data)
+// {
+//     uint16_t touchX, touchY;
+
+//     bool touched = tft.getTouch(&touchX, &touchY, 600);
+
+//     if (!touched)
+//     {
+//         data->state = LV_INDEV_STATE_REL;
+//     }
+//     else
+//     {
+//         data->state = LV_INDEV_STATE_PR;
+
+//         /*Set the coordinates*/
+//         data->point.x = touchX;
+//         data->point.y = touchY;
+
+//         Serial.print("Data x ");
+//         Serial.println(touchX);
+
+//         Serial.print("Data y ");
+//         Serial.println(touchY);
+//     }
+// }
 
 void setup()
 {
-    pinMode(BLK, OUTPUT);
-    digitalWrite(BLK, LOW);
+    Serial.begin(115200); /* prepare for possible serial debug */
 
-    /* TFT init */
-    tft.init(ST7789_DRIVER);
-    tft.setRotation(0);
+    String LVGL_Arduino = "Hello Arduino! ";
+    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
 
-    /*初始化显示*/
-    lv_init();                                                                 // 初始化lvgl库
-    lv_disp_draw_buf_init(&draw_buf, buf_1, NULL, screenWidth * screenHeight); // 开启双缓冲
+    Serial.println(LVGL_Arduino);
+    Serial.println("I am LVGL_Arduino");
+
+    lv_init();
+
+#if LV_USE_LOG != 0
+    lv_log_register_print_cb(my_print); /* register print function for debugging */
+#endif
+
+    tft.begin();        /* TFT init */
+    tft.setRotation(3); /* Landscape orientation, flipped */
+
+    /*Set the touchscreen calibration data,
+     the actual data for your display can be acquired using
+     the Generic -> Touch_calibrate example from the TFT_eSPI library*/
+    // uint16_t calData[5] = {275, 3620, 264, 3532, 1};
+    // tft.setTouch(calData);
+
+    lv_disp_draw_buf_init(&draw_buf, buf, NULL, screenWidth * screenHeight / 10);
 
     /*Initialize the display*/
     static lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
-
     /*Change the following line to your display resolution*/
-
     disp_drv.hor_res = screenWidth;
     disp_drv.ver_res = screenHeight;
-    disp_drv.flush_cb = my_disp_flush; /*Set your driver function*/
-    disp_drv.draw_buf = &draw_buf;     /*Assign the buffer to the display*/
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register(&disp_drv);
 
-    setup_ui(&guider_ui);
+    // /*Initialize the (dummy) input device driver*/
+    // static lv_indev_drv_t indev_drv;
+    // lv_indev_drv_init(&indev_drv);
+    // indev_drv.type = LV_INDEV_TYPE_POINTER;
+    // indev_drv.read_cb = my_touchpad_read;
+    // lv_indev_drv_register(&indev_drv);
 
-    // lv_disp_drv_register(&disp_drv);   // 注册显示屏
+    /* Create simple label */
+    lv_obj_t *label = lv_label_create(lv_scr_act());
+    lv_label_set_text(label, "Hello Ardino and LVGL!");
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 
-    // screenMain = lv_obj_create(NULL); // 创建一个实际的屏幕对象
-    // labelName = lv_label_create(screenMain);
-    // // labelFans = lv_label_create(screenMain);87
-    // labelTime = lv_label_create(screenMain);
-    // labelLocation = lv_label_create(screenMain);
-    // labelWeather = lv_label_create(screenMain);
-    // labelTemperature = lv_label_create(screenMain);
-    // // imgHeadPhoto = lv_img_create(screenMain);
+    /* Try an example. See all the examples
+     * online: https://docs.lvgl.io/master/examples.html
+     * source codes: https://github.com/lvgl/lvgl/tree/e7f88efa5853128bf871dde335c0ca8da9eb7731/examples */
+    // lv_example_btn_1();
 
-    digitalWrite(BLK, HIGH);
+    /*Or try out a demo. Don't forget to enable the demos in lv_conf.h. E.g. LV_USE_DEMOS_WIDGETS*/
+    // lv_demo_widgets();
+    //  lv_demo_benchmark();
+    //  lv_demo_keypad_encoder();
+    //  lv_demo_music();
+    //  lv_demo_printer();
+    //  lv_demo_stress();
 
-    // lv_label_set_long_mode(labelName, LV_LABEL_LONG_WRAP);
-    // lv_label_set_text(labelName, "Hello! Yogurt");
-    // lv_obj_set_size(labelName, 120, 30);
-    // lv_obj_align(labelName, LV_ALIGN_DEFAULT, 120, 120);
-    // lv_obj_set_style_text_font(labelName, &lv_font_montserrat_14, 0);
-
-    // lv_label_set_long_mode(labelLocation, LV_LABEL_LONG_WRAP);
-    // lv_label_set_text_fmt(labelLocation, "城市: %s", location);
-    // lv_obj_set_size(labelLocation, 120, 20);
-    // lv_obj_align(labelLocation, LV_ALIGN_DEFAULT, 0, 40);
-    // lv_obj_set_style_text_font(labelLocation, &lv_font_montserrat_14, 0);
-
-    // lv_label_set_long_mode(labelWeather, LV_LABEL_LONG_WRAP);
-    // lv_label_set_text_fmt(labelWeather, "天气: %s", weather);
-    // lv_obj_set_size(labelWeather, 120, 20);
-    // lv_obj_align(labelWeather, LV_ALIGN_DEFAULT, 0, 60);
-    // lv_obj_set_style_text_font(labelWeather, &lv_font_montserrat_14, 0);
-
-    // lv_label_set_long_mode(labelTemperature, LV_LABEL_LONG_WRAP);
-    // lv_label_set_text_fmt(labelTemperature, "气温: %s度", temperature);
-    // lv_obj_set_size(labelTemperature, 120, 20);
-    // lv_obj_align(labelTemperature, LV_ALIGN_DEFAULT, 0, 80);
-    // lv_obj_set_style_text_font(labelTemperature, &lv_font_montserrat_14, 0);
-
-    // lv_img_set_src(imgHeadPhoto, &head_photo);
-    // lv_obj_align(imgHeadPhoto, LV_ALIGN_CENTER, 64, 64);
-    // lv_obj_set_size(imgHeadPhoto, 60, 60);
+    Serial.println("Setup done");
 }
+
 void loop()
 {
-    // lv_scr_load(&guider.screen);
-
-    lv_task_handler();
-    vTaskDelay(1);
+    lv_timer_handler(); /* let the GUI do its work */
+    delay(5);
 }
